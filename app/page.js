511e928f519fc27113e44f07cc017c840def801b
aryandeulkar1 <aryandeulkar18@gmail.com>
2025-10-10 21:07:40 -0400

    first commit

---
+++ app/page.js
@@ -0,0 +1,38 @@
+"use client";
+
+import StaggeredMenu from '../components/StaggeredMenu';
+
+const menuItems = [
+  { label: 'Home', ariaLabel: 'Go to home page', link: '/' },
+  { label: 'About', ariaLabel: 'Learn about us', link: '/about' },
+  { label: 'Services', ariaLabel: 'View our services', link: '/services' },
+  { label: 'Contact', ariaLabel: 'Get in touch', link: '/contact' },
+];
+
+const socialItems = [
+  { label: 'Twitter', link: 'https://twitter.com' },
+  { label: 'GitHub', link: 'https://github.com' },
+  { label: 'LinkedIn', link: 'https://linkedin.com' },
+];
+
+export default function Home() {
+  return (
+    <div style={{ height: '100vh', background: '#1a1a1a' }}>
+      <StaggeredMenu
+        position="right"
+        items={menuItems}
+        socialItems={socialItems}
+        displaySocials={true}
+        displayItemNumbering={true}
+        menuButtonColor="#fff"
+        openMenuButtonColor="#4c13e7ff"
+        changeMenuColorOnOpen={true}
+        colors={['#B19EEF', '#5227FF']}
+        logoUrl="/path-to-your-logo.svg"
+        accentColor="#d8db24ff"
+        onMenuOpen={() => console.log('Menu opened')}
+        onMenuClose={() => console.log('Menu closed')}
+      />
+    </div>
+  );
+}
